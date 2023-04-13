5e83d0e51a3732b2bee736afde4183f051f7671d
Sid K <siddarthkrishnan.99@live.com>
2023-04-13 11:28:30 -0400

    modify links for nextjs legacy behaviour

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -4,7 +4,7 @@
 
 import { useWeb3 } from "@components/providers"
 import Link from "next/link"
-import { Button } from "@components/ui/common"
+import { ActiveLink, Button } from "@components/ui/common"
 import { useAccount } from "@components/hooks/web3"
 import { useRouter } from "next/router"
 
@@ -19,24 +19,32 @@ export default function Navbar() {
         <nav className="relative" aria-label="Global">
           <div className="flex justify-between items-center">
             <div>
-              <Link href="/"
-                  className="font-medium mr-8 text-gray-500 hover:text-gray-900">
-                  Home
-              </Link>
-              <Link href="/marketplace"
+              <ActiveLink href="/">
+                <a  
+                    className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                    Home
+                </a>
+              </ActiveLink>
+              <ActiveLink href="/marketplace">
+                <a
                   className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                   Marketplace
-              </Link>
-              <Link href="/"
+                </a>
+              </ActiveLink>
+              <ActiveLink href="/blogs">
+                <a
                   className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                   Blogs
-              </Link>
+                </a>
+              </ActiveLink>
             </div>
             <div>
-              <Link href="/"
+              <ActiveLink href="/wishlist">
+                <a
                   className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                   Wishlist
-              </Link>
+                </a>
+              </ActiveLink>
               { isLoading ?
                 <Button
                   disabled={true}
