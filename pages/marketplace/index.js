79009cac814ab13a38763bc2e3493a921b49198b
Sid K <siddarthkrishnan.99@live.com>
2023-04-16 20:29:11 -0400

    finishing order form

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -13,6 +13,9 @@ import { MarketHeader } from "@components/ui/marketplace"
 export default function Marketplace({courses}) {
   const [selectedCourse, setSelectedCourse] = useState(null)
   const { canPurchaseCourse } = useWalletInfo()
+  const purchaseCourse = (order) => {
+    alert(JSON.stringify(order))
+  }
   return (
     <>
       <div className="py-4">
@@ -42,6 +45,7 @@ export default function Marketplace({courses}) {
       { selectedCourse && 
         <OrderModal 
           course={selectedCourse}
+          onSubmit = {purchaseCourse}
           onClose={() => setSelectedCourse(null)}
         />
       }
